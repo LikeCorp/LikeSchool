e5f547c78aa758f2c4a87e2ed830f7bda077a0e4
lokeshsuhas <lokeshsuhas@gmail.com>
2012-09-13 14:30:49 -0500

    Updated the helper methods.

--- LikeSchool.Helpers/HelperMethods.cs
+++ LikeSchool.Helpers/HelperMethods.cs
@@ -7,11 +7,21 @@ namespace LikeSchool.Helpers
 {
     public static class HelperMethods
     {
-        public static string GetSubString(string firstIndexString, string lastIndexString,string originalValue)
+        public static string GetSubString(string firstIndexString, string lastIndexString, string originalValue)
         {
-            int startIndex = originalValue.LastIndexOf(firstIndexString)+1;
+            int startIndex = originalValue.LastIndexOf(firstIndexString) + 1;
             int lastIndex = originalValue.LastIndexOf(lastIndexString);
-            return originalValue.Substring(startIndex, lastIndex-startIndex);
+            return originalValue.Substring(startIndex, lastIndex - startIndex);
+        }
+        public static string ToUpperCaseFirst(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+            char[] strArray = str.ToCharArray();
+            strArray[0] = char.ToUpper(strArray[0]);
+            return new string(strArray);
         }
     }
 }
